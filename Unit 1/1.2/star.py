a500ef259ceccc156b9a7bee41ca30395c7ae86a
Jacob Johnson <Jacob.Johnson@NeuAnalytics.com>
2017-09-14 15:43:27 -0500

    Added in Unit 1.2 Studios

---
+++ Unit 1/1.2/star.py
@@ -0,0 +1,37 @@
+import turtle
+
+def draw_line(angle):
+    mike = turtle.Turtle()
+    mike.left(angle)
+    mike.left(90)
+    mike.forward(90)
+    mike.left(90)
+    mike.forward(90)
+    mike.left(90)
+    mike.forward(90)
+    mike.left(90)
+    mike.forward(180)
+    mike.right(90)
+    mike.forward(90)
+    mike.right(90)
+    mike.forward(90)
+    mike.right(90)
+    mike.forward(180)
+    mike.right(90)
+    mike.forward(90)
+    mike.right(90)
+    mike.forward(90)
+    mike.right(90)
+    mike.forward(180)
+    mike.left(90)
+    mike.forward(90)
+    mike.left(90)
+    mike.forward(90)
+    mike.left(90)
+    mike.forward(90)
+
+def square(lines):
+    for angle in range(0, 180, int(180/lines)):
+        draw_line(angle)
+        
+square(10)
